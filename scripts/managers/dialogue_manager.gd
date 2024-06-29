a491d5010630aba6dd823750e94804ca57e5b207
= <calcopoddev@gmail.com>
2024-06-29 15:06:00 +0300

    Coroutine support for parameters, dialogue basics.

--- scripts/managers/dialogue_manager.gd
+++ scripts/managers/dialogue_manager.gd
@@ -3,11 +3,15 @@ extends Node
 
 static var instance: DialogueManager = null
 
+@export_group("Reference")
+@export var anim: AnimationPlayer = null
 @export var text_display: RichTextLabel = null
 @export var dialogue_box: Control = null
 
-@export var is_hidden: bool = true:
+@export var is_hidden: bool = false:
 	set(value):
+		if is_hidden == value:
+			return
 		is_hidden = value
 		if value:
 			hide()
@@ -17,20 +21,31 @@ static var instance: DialogueManager = null
 func display() -> void:
 	if not is_hidden:
 		return
-	
-	dialogue_box.visible = true
+	is_hidden = false
+	# dialogue_box.visible = true
+	anim.play("dialogue_enter")
+	await anim.animation_finished
 
 func hide() -> void:
 	if is_hidden:
 		return
-	
-	dialogue_box.visible = false
+	is_hidden = true
+	# dialogue_box.visible = false
+	anim.play("dialogue_exit")
+	await anim.animation_finished
 
 func play_text(text: String) -> void:
-	print("Started dialogue")
+	display()
 	var coro = Coro.new(play_text_coroutine, true)
 	await coro.run({"text": text, "cps": 10})
-	print("Ended dialogue")
+	await await_player_input("DIALOGUE_NEXT")
+	hide()
+
+func await_player_input(action):
+	while true:
+		await Game.on_pre_process
+		if Input.is_action_just_pressed(action):
+			break
 
 func play_text_coroutine(coro: Coro, params: Dictionary) -> void:
 	var text = params["text"]
@@ -53,3 +68,4 @@ func _enter_tree() -> void:
 		queue_free()
 		return
 	instance = self
+	hide()
